c7eff5c534784a37118b82fa867c9af78e1a9688
gousse_p <pierre-henri.gousse@epitech.eu>
2015-10-19 22:43:12 +0200

    Complete Interface ISocket

--- cpp_ABSocket/ISocket.hh
+++ cpp_ABSocket/ISocket.hh
@@ -1,7 +1,14 @@
 #pragma once
+
+#include <string>
+
 class ISocket
 {
 	public:
-
+		virtual void	init() = 0;
+		virtual bool	connectToServer(std::string const &host, short port) = 0;
+		virtual int		recv(std::string &buffer, int size) = 0;
+		virtual int		send(std::string const &data) = 0;
+		virtual void	end() = 0;
 };
 
